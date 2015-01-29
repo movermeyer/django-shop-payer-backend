af6f341a82381966ba04a8975143687268c7aa40
Simon Fransson <simon@dessibelle.se>
2015-01-29 22:18:14 +0100

    Added swedish i18n.

--- django_shop_payer_backend/__init__.py
+++ django_shop_payer_backend/__init__.py
@@ -6,7 +6,6 @@ try:
     from django.core.checks import register
     from django.core.checks import Error, Info
 
-    from django.utils.translation import ugettext as _
     from django.conf import settings
 
     @register()
@@ -17,24 +16,24 @@ try:
 
             if not hasattr(settings, 'SHOP_PAYER_BACKEND_AGENT_ID') or not getattr(settings, 'SHOP_PAYER_BACKEND_AGENT_ID', None):
                 errors.append(Info(
-                    _('Payer Agent ID not in settings.'),
-                    hint=_('Add a setting property for the key `SHOP_PAYER_BACKEND_AGENT_ID` containing the Agent ID for the Payer API.'),
+                    'Payer Agent ID not in settings.',
+                    hint='Add a setting property for the key `SHOP_PAYER_BACKEND_AGENT_ID` containing the Agent ID for the Payer API.',
                     obj=__name__,
                     id='django_shop_payer_backend.E001',
                 ))
 
             if not hasattr(settings, 'SHOP_PAYER_BACKEND_ID1') or not getattr(settings, 'SHOP_PAYER_BACKEND_ID1', None):
                 errors.append(Info(
-                    _('Payer Key 1 not in settings.'),
-                    hint=_('Add a setting property for the key `SHOP_PAYER_BACKEND_ID1` containing the Key 1 for the Payer API.'),
+                    'Payer Key 1 not in settings.',
+                    hint='Add a setting property for the key `SHOP_PAYER_BACKEND_ID1` containing the Key 1 for the Payer API.',
                     obj=__name__,
                     id='django_shop_payer_backend.E002',
                 ))
 
             if not hasattr(settings, 'SHOP_PAYER_BACKEND_ID2') or not getattr(settings, 'SHOP_PAYER_BACKEND_ID2', None):
                 errors.append(Info(
-                    _('Payer Key 2 not in settings.'),
-                    hint=_('Add a setting property for the key `SHOP_PAYER_BACKEND_ID2` containing the Key 2 for the Payer API.'),
+                    'Payer Key 2 not in settings.',
+                    hint='Add a setting property for the key `SHOP_PAYER_BACKEND_ID2` containing the Key 2 for the Payer API.',
                     obj=__name__,
                     id='django_shop_payer_backend.E003',
                 ))
